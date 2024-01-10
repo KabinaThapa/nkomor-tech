94d1b6e8ad7dc7d717a001c501c4c5814254fdc9
Kabina Thapa <thapakabina28@gmail.com>
2024-01-09 19:41:12 -0500

    add logo to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import brandlogo from '../assets/images/brandlogo.png'
+import { IoIosArrowDown } from "react-icons/io";
 const navbar = () => {
   return (
-    <div>navbar</div>
+    <div className='[1440px] border-2 bg-slate-300 h-[120px] '>
+<div className='relative w-full h-[88px] border mt-8 bg-customgreen flex'>
+    <div className='w-[169px] h-[50px] mt-[20px] ml-[38px] border '>
+        <img src={brandlogo} alt='brandlogo' className='object-fit w-full h-full text-black '/>
+    </div>
+    <div className='absolute w-[48px] h-[48px] mt-[20px] ml-[1152px] border rounded-full'>
+NP
+    </div>
+<div className='absolute w-[122px] h-[24px] mt-[33px] ml-[1213px] '>
+    <p className='text-[20px]'>Noah Parker</p>
+</div>
+<div className='absolute w-[24px] h-[24px] top-[33px] ml-[1351px] border'>
+<IoIosArrowDown className='w-full h-full' />
+</div>
+
+</div>
+    </div>
   )
 }
 
