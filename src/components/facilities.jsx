2e2149355e6b3e0bb00059a25219900301aea7cd
Kabina Thapa <thapakabina28@gmail.com>
2024-02-06 15:36:48 -0500

    chore:update facilities component by adding 'verified for booking'

--- src/components/facilities.jsx
+++ src/components/facilities.jsx
@@ -6,6 +6,7 @@ import hollard from '../assets/images/hollard.svg'
 import hospitalimg from '../assets/images/hospitalimage.svg'
 import pin from '../assets/images/pin.svg'
 import schedule from '../assets/images/schedule.svg'
+import vector from '../assets/images/Vector.png'
 
 
 const facilities = () => {
@@ -22,18 +23,41 @@ const facilities = () => {
        address:'Castle Rd, Accra, Ghana',
    openHours:'MON-SUN 10:00AM-11.59PM',
 image:{hospitalimg}},
+{id:3,
+    name:'Greater Accra Regional Hospital', 
+    insurance:{img1:bima, img2:acacia, img3:alica, img4:hollard},
+   address:'Castle Rd, Accra, Ghana',
+openHours:'MON-SUN 10:00AM-11.59PM',
+image:{hospitalimg}},
+{id:4,
+    name:'Greater Accra Regional Hospital', 
+    insurance:{img1:bima, img2:acacia, img3:alica, img4:hollard},
+   address:'Castle Rd, Accra, Ghana',
+openHours:'MON-SUN 10:00AM-11.59PM',
+image:{hospitalimg}},
+{id:5,
+    name:'Greater Accra Regional Hospital', 
+    insurance:{img1:bima, img2:acacia, img3:alica, img4:hollard},
+   address:'Castle Rd, Accra, Ghana',
+openHours:'MON-SUN 10:00AM-11.59PM',
+image:{hospitalimg}},
     ]
 
   return (
    <>
+   <div className='flex flex-col justify-center items-center w-full border '>
 {
     facilities.map((item)=>(
-        <div className='facilities w-[1029px] h-[329px] border grid grid-cols-3 items-center mx-auto mb-8 gap-4 rounded-[20px]'>
-            <div className='flex items-center justify-end border '>
-                <img className='w-[185px] h-[186px] rounded-[8px]' src={hospitalimg}/>
+        <div className='facilities  w-[1022px] h-[329px] p-8 border border-red-500 flex items-center justify-between gap-6 mb-8  rounded-[20px]'>
+            <div className='flex  border w-[185px] h-[186px]  '>
+                <img className='w-full h-full rounded-[8px]  ' src={hospitalimg}/>
             </div>
-            <div className='flex flex-col col-span-2 border '>
+            <div className='flex flex-col w-full  border border-red-600'>
+                <div className='flex items-center gap-4'>
                 <p className='text-[32px] font-[600] leading-[40px]'>{item.name}</p>
+                <p className='flex items-center gap-1 font-inter font-[500] text-[16px] leading-[32px]'>Verified for booking <img className='w-[20px] h-[20px]' src={vector}/> </p>
+                </div>
+               
             <div className='flex gap-[54px] items-center border  mt-[28px] mb-[28px]'>
     
     <img className='w-[82.3px] h-[26.6px]' src={item.insurance.img1}/>
@@ -54,6 +78,7 @@ image:{hospitalimg}},
         </div>
     ))
 }
+</div>
    </>
   )
 }
--- src/pages/appointmentPage.jsx
+++ src/pages/appointmentPage.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import Facilities from '../components/facilities'
+import Navbar from '../components/navbar'
+import Sidenav from '../components/sidenav'
 
 const appointmentPage = () => {
   return (
-    <Facilities/>
+    <>
+    <Navbar/>
+    <div className='flex'>
+    <Sidenav/>
+     <Facilities/>
+    </div>
+   
+    </>
+   
   )
 }
 
